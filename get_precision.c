9d40207bce6a6d0bc5e2a167a925fe5cd259f34d
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-04-22 04:41:31 -0700

    Teamwork

--- get_precision.c
+++ get_precision.c
@@ -1,51 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
 
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
-int get_precision(const char *format)
+int get_precision(const char *format, int *i, va_list list)
 {
-int precision = -1;
-int i = 0;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-while (format[i] != '\0')
-{
-if (format[i] == '.')
-{
-i++;
-if (isdigit(format[i]))
-{
-precision = atoi(&format[i]);
-}
-else
-{
-precision = 0;
-}
-break;
-}
-i++;
-}
+	if (format[curr_i] != '.')
+		return (precision);
 
-return (precision);
-}
+	precision = 0;
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-const char *format1 = "%.2f";
-const char *format2 = "%s";
-const char *format3 = "%.0f";
-printf("Precision for format1: %d\n", get_precision(format1));
-printf("Precision for format2: %d\n", get_precision(format2));
-printf("Precision for format3: %d\n", get_precision(format3));
-return (0);
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (precision);
 }
