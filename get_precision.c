ef2b30894f534553c3be32f713208ca526ba3385
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-04-22 04:45:18 -0700

    Teamwork

--- get_precision.c
+++ get_precision.c
@@ -3,10 +3,10 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Pointer to the current index of the format string.
+ * @list: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: The precision to be used for printing.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
